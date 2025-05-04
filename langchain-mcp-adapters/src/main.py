207c49a7c0976fb6f40034b5fa5e413fe74602b2
Gall-oDrone <piehavok@hotmail.com>
2025-05-03 18:38:23 -0600

    Imports Hands On

--- langchain-mcp-adapters/src/main.py
+++ langchain-mcp-adapters/src/main.py
@@ -1,9 +1,21 @@
 import asyncio
 import os
 from dotenv import load_dotenv
-
 load_dotenv()
 
+from mcp import ClientSession,StdioServerParameters
+from mcp.client.stdio import stdio_client
+
+from langchain_openai import ChatOpenAI
+from langchain_mcp_adapters import load_mcp_tools
+from langgraph.prebuilt import create_react_agent
+
+llm = ChatOpenAI()
+
+stdio_server_params = StdioServerParameters(
+    command="python",
+    args=["/src/servers/math_server.py"]
+)
 async def main():
     print("Hello world")
 
