334172bcd7b4afcdbf030bb6a23b523a232b0001
Gall-oDrone <piehavok@hotmail.com>
2025-05-03 19:36:08 -0600

    Hands on - Connecting MCP Client to Server and Initialize Session with LangGraph

--- langchain-mcp-adapters/src/main.py
+++ langchain-mcp-adapters/src/main.py
@@ -1,15 +1,14 @@
 import asyncio
 import os
 from dotenv import load_dotenv
-load_dotenv()
-
 from mcp import ClientSession,StdioServerParameters
 from mcp.client.stdio import stdio_client
-
 from langchain_openai import ChatOpenAI
-from langchain_mcp_adapters import load_mcp_tools
+from langchain_mcp_adapters.tools import load_mcp_tools
 from langgraph.prebuilt import create_react_agent
+from langchain_core.messages import HumanMessage
 
+load_dotenv()
 llm = ChatOpenAI()
 
 stdio_server_params = StdioServerParameters(
@@ -17,7 +16,19 @@ stdio_server_params = StdioServerParameters(
     args=["/src/servers/math_server.py"]
 )
 async def main():
-    print("Hello world")
+    async with stdio_client(stdio_server_params) as (read,write):
+        async with ClientSession(read_stream=read,write_stream=write) as session:
+            await session.initialize()
+            print("session initialized")
+            tools = await load_mcp_tools(session)
+            print("tools: ", tools)
+
+            agent = create_react_agent(llm,tools)
+
+            result = await agent.ainvoke({
+                "messages": [HumanMessage(content="what is 2 * 2?")]
+            })
+            print(result["messages"][-1].content)
 
 if __name__ == "__main__":
     asyncio.run(main())
